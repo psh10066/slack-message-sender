e1eabc5274abb0cc078bbdb823d6cf898aef8ab8
이호준 <hj.lee@60hz.io>
2024-07-29 21:19:51 +0900

    프로젝트 생성

---
+++ src/main/java/com/psh10066/slackmessagesender/SlackMessageSenderApplication.java
@@ -0,0 +1,13 @@
+package com.psh10066.slackmessagesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackMessageSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackMessageSenderApplication.class, args);
+    }
+
+}
